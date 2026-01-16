62176d200d262b38048ea95935fef2a31d7e0187
vasi <alexvasilescu25@gmail.com>
2026-01-16 11:51:46 +0100

    updated url

--- commands.js
+++ commands.js
@@ -82,7 +82,7 @@ function checkRecipients() {
         //     }
         // });
         // fetch('http://localhost:8000/api/email/list')
-        fetch('https://tired-sloths-cross.loca.lt')
+        fetch('https://vasiemailvalidator.loca.lt')
             .then(response => response.json())
             .then(validEmails => {
                 // validEmails is now the array from your API
